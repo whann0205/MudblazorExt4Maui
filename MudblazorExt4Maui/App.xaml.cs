49d4bb6b656b68488100dc0cbb77af8fbaa97802
whann0205 <71807453+whann0205@users.noreply.github.com>
2024-08-26 13:56:14 +0200

    Add project files.

---
+++ MudblazorExt4Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MudblazorExt4Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MudblazorExt4Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MudblazorExt4Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
